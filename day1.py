82267a05cd52b8ca8a8ac4c96f53636311a989f1
Hassan Qadeer <hassanqadeer007@gmail.com>
2022-12-03 01:46:40 +0400

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,23 @@
+from re import A
+
+
+print("Hello") #function print used for print something.
+''' cannot be shown as these are comments in
+
+
+
+
+ triple commas''' 
+
+a=20
+print(a)
+a="hassan is ---"
+print(a)
+
+a=input("enter the name")
+#a=int(a)
+b=a*2
+print(b,"  ",b)
+c=3
+c-=c
+print (c)
